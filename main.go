bf3b32e77acc1f660e9c63cc06962bc6f25b08ad
Altria <fremustang@qq.com>
2022-05-14 21:22:02 +0800

    能跑的demo

--- main.go
+++ main.go
@@ -6,8 +6,7 @@ import (
 
 func main() {
 	r := gin.Default()
-
 	initRouter(r)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("10.0.20.8:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- router.go
+++ router.go
@@ -8,7 +8,7 @@ import (
 func initRouter(r *gin.Engine) {
 	// public directory is used to serve static resources
 	r.Static("/static", "./public")
-
+	
 	apiRouter := r.Group("/douyin")
 
 	// basic apis
