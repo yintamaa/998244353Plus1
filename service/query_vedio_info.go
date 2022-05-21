b40a0c8ab977f8b04da17e0d74fa12eefab7aa27
bitsetli <bitsetli@tencent.com>
2022-05-21 22:17:37 +0800

    添加主体逻辑

---
+++ service/query_vedio_info.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"github.com/yintamaa/998244353Plus1/repository"
+	"github.com/yintamaa/998244353Plus1/util"
+)
+
+type VedioInfo struct {
+	vedios []*repository.Vedio
+}
+
+var next_time int64
+
+func QueryVedioInfoFlow() (*repository.Vedio, error) {
+	var feed_query_flow VedioInfo
+	feed_query_flow.Do()
+	return &feed_query_flow.vedios, nil
+}
+
+func (f *VedioInfo) Do() (*VedioInfo, error) {
+	if err := f.checkParam(); err != nil {
+		return nil, err
+	}
+	if err := f.prepareInfo(); err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+func (f *VedioInfo) checkParam() error {
+	return nil
+}
+func (f *VedioInfo) prepareInfo() error {
+	vedios, err := repository.NewVedioDaoInstance().QueryVedioRecent(1)
+	if err != nil {
+		util.Logger.Error(":" + err.Error())
+	}
+	f.vedios = vedios
+	next_time = vedios[0].CreateTime
+	return nil
+}
